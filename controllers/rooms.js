c2be77dc9a15f5ea249734ff66b12d97141d675e
Rohan <kumarsrohan2@gmail.com>
2025-07-20 08:55:04 +0530

    Room

--- controllers/rooms.js
+++ controllers/rooms.js
@@ -1,6 +1,25 @@
 const Room = require("../models/rooms.js"); 
 
+
 module.exports.showRooms = async (req, res) => {
   const rooms = await Room.find({});
-  res.render("category/roomslistings", { rooms }); // Pass rooms to EJS
+  res.render("category/rooms/roomslistings", { rooms }); // Pass rooms to EJS
+};
+
+
+module.exports.showRoom = async(req,res)=>{
+    const {id} = req.params;
+
+    const room = await Room.findById(id).populate({path:"reviews",
+        populate:{path:"author"},
+      })
+      .populate("owner");
+  
+
+    if(!room){
+        req.flash("error","The room you have requested Does not exists!");
+        return res.redirect("/rooms");
+    }
+    res.render("category/rooms/room.ejs",{room})
+
 };
--- routes/rooms.js
+++ routes/rooms.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const roomsController = require("../controllers/rooms.js")
 
-
 // GET /Rooms - Show all rooms
-router.get("/Rooms",roomsController.showRooms);
+router.get("/rooms",roomsController.showRooms);
+
+router.get("/room/:id",roomsController.showRoom);
 
 module.exports = router;
