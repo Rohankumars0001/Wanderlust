3fd0511950ceced149058482ebbf5caf033b6570
Rohan <kumarsrohan2@gmail.com>
2025-08-07 23:01:31 +0530

    done with forms

--- controllers/listings.js
+++ controllers/listings.js
@@ -59,7 +59,16 @@ module.exports.renderEditForm = async (req, res) => {
       req.flash("error", "Listing you requested does not exist");
       return res.redirect("/listings");
     }
-    res.render("listings/edit.ejs", { listing });
+
+    const originalImageUrl = listing?.image?.url;
+
+    if (typeof originalImageUrl === "string") {
+      const thumbnailUrl = originalImageUrl.replace("/upload", "/upload/h_300,w_250");
+    } else {
+      console.error("Image URL is invalid", originalImageUrl);
+    }
+
+    res.render("listings/edit.ejs", { listing, originalImageUrl });
   };
 
 module.exports.updateListing = async (req, res) => {
